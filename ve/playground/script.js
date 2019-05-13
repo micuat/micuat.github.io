2bcb1eeb689401efa742c1b25541e815a103b01e
micuat <micuat@gmail.com>
2019-05-13 22:38:50 +0200

    3js

--- ve/playground/script.js
+++ ve/playground/script.js
@@ -1,17 +1,17 @@
 const ac = new AudioContext();
-var osc = null;
-var gainNode = ac.createGain();
-var gainNode2 = ac.createGain();
+let osc = null;
+const gainNode = ac.createGain();
+const gainNode2 = ac.createGain();
 
-var bufferSize = 2 * ac.sampleRate,
+const bufferSize = 2 * ac.sampleRate,
     noiseBuffer = ac.createBuffer(1, bufferSize, ac.sampleRate),
     output = noiseBuffer.getChannelData(0);
-for (var i = 0; i < bufferSize; i++) {
+for (let i = 0; i < bufferSize; i++) {
     output[i] = Math.random() * 2 - 1;
 }
 
 // https://noisehack.com/generate-noise-web-audio-api/
-var whiteNoise = ac.createBufferSource();
+const whiteNoise = ac.createBufferSource();
 whiteNoise.buffer = noiseBuffer;
 whiteNoise.loop = true;
 whiteNoise.start(0);
@@ -54,7 +54,7 @@ gainNode2.gain.value = 0;
 // distortion.curve = makeDistortionCurve(400);
 // distortion.oversample = '4x';
 
-var biquadFilter = ac.createBiquadFilter();
+const biquadFilter = ac.createBiquadFilter();
 biquadFilter.connect(gainNode2);
 
 document.addEventListener('click', function(event) {
@@ -68,43 +68,43 @@ document.addEventListener('click', function(event) {
   }
 });
 
-var seq = [2, 3, 4, 0, 2, 0, 4, 5];//, 2, 3, 4, 0, 2, 0, 4, 5];
-var offset = [0.1, 0.1, 0.3, 0.1, 0.1, 0.3, 0.1, 0.1];
-var index = 0;
-var lastT = 0;
-var lastT2 = 0;
+const seq = [2, 3, 4, 0, 2, 0, 4, 5];//, 2, 3, 4, 0, 2, 0, 4, 5];
+const offset = [0.1, 0.1, 0.3, 0.1, 0.1, 0.3, 0.1, 0.1];
+let index1 = 0;
+let lastT = 0;
+let lastT2 = 0;
 
-var seq2 = [1, 1, 0, 1, 1, 0, 1, 1];
-var offset2 = [0.1, 0.1, 0.3, 0.1, 0.1, 0.3, 0.1, 0.1];
-var index2 = 0;
+const seq2 = [1, 1, 0, 1, 1, 0, 1, 1];
+const offset2 = [0.1, 0.1, 0.3, 0.1, 0.1, 0.3, 0.1, 0.1];
+let index2 = 0;
 
 setInterval(() => {
   let d = new Date();
   let t = d.getTime();
-  if(Math.floor(t/100+offset[index]) - Math.floor(lastT/100+offset[(index+offset.length-1)%offset.length]) > 0) {
+  if(Math.floor(t/100+offset[index1]) - Math.floor(lastT/100+offset[(index1+offset.length-1)%offset.length]) > 0) {
     lastT = t;
     if(osc) {
       osc.stop();
       osc.disconnect();
       osc = ac.createOscillator();
     
-      // osc.type = 'square';
+      osc.type = 'square';
       osc.connect(gainNode);
     
       osc.start();
-      osc.frequency.value = 400 + seq[index] / 6 * 3000;
-      if(seq[index] > 0)
+      osc.frequency.value = 400 + seq[index1] / 6 * 3000;
+      if(seq[index1] > 0)
         gainNode.gain.value = 0.3;
       else
         gainNode.gain.value = 0;
-      index = (index + 1) % seq.length;
+      index1 = (index1 + 1) % seq.length;
       gainNode.gain.setTargetAtTime(0, ac.currentTime, 0.075);
 
       if(Math.random() > 0.85) {
         if(Math.random() > 0.1)
-          seq[index] = Math.floor(Math.random()*4) + 1;
-        else seq[index] = 0;
-        offset[index] = Math.floor(Math.random() * 4) * 0.1;
+          seq[index1] = Math.floor(Math.random()*4) + 1;
+        else seq[index1] = 0;
+        offset[index1] = Math.floor(Math.random() * 4) * 0.1;
       }
     }
   }
@@ -129,3 +129,106 @@ setInterval(() => {
     }
   }
 }, 5);
+
+{
+// three.js animataed line using BufferGeometry
+
+let renderer, scene, camera;
+
+let line;
+const MAX_POINTS = 500;
+let drawCount;
+let tween = 0;
+
+init();
+animate();
+
+function init() {
+	// renderer
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	// scene
+	scene = new THREE.Scene();
+
+	// camera
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.set( 0, 0, 1000 );
+
+	// geometry
+	var geometry = new THREE.BufferGeometry();
+
+	// attributes
+	var positions = new Float32Array( MAX_POINTS * 3 ); // 3 vertices per point
+	geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+
+	// drawcalls
+	drawCount = 2; // draw the first 2 points, only
+	geometry.setDrawRange( 0, drawCount );
+
+	// material
+	var material = new THREE.LineBasicMaterial( { color: 0xff0000, linewidth: 2 } );
+
+	// line
+	line = new THREE.Line( geometry,  material );
+	scene.add( line );
+
+	// update positions
+	updatePositions();
+
+}
+
+// update positions
+function updatePositions() {
+  tween = seq[index1] * 0.5 + 0.5 * tween;
+	let positions = line.geometry.attributes.position.array;
+
+	let x = y = z = index = 0;
+
+	for ( let i = 0, l = MAX_POINTS; i < l; i ++ ) {
+		x = Math.cos(i*0.1+drawCount*0.1) * 50 * tween;
+		y = Math.sin(i*0.1+drawCount*0.1) * 50 * tween;
+		z = 0;
+
+		positions[ index ++ ] = x;//*0.01+0.99*positions[ index ];
+		positions[ index ++ ] = y;//*0.01+0.99*positions[ index ];
+		positions[ index ++ ] = z;//*0.01+0.99*positions[ index ];
+
+
+	}
+
+}
+
+// render
+function render() {
+
+	renderer.render( scene, camera );
+
+}
+
+// animate
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	drawCount = ( drawCount + 1 ) % MAX_POINTS;
+
+	line.geometry.setDrawRange( 0, MAX_POINTS );
+
+
+		// periodically, generate new data
+
+		updatePositions();
+
+		line.geometry.attributes.position.needsUpdate = true; // required after the first render
+
+		line.material.color.setHSL( 0.7, 1, 0.5 );
+
+	
+
+	render();
+
+}
+}
