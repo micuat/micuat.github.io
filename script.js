9c2c3cefa92cbf337a950bcfc08606b16a3dc89d
micuat <micuat@gmail.com>
2021-05-09 00:53:41 +0200

    wat

--- script.js
+++ script.js
@@ -15,7 +15,12 @@ hydra = new Hydra({
   height: 512
 });
 
+s0.initVideo("./img/bp.webm");
 osc(20,0.02,1.5).rotate(0.1)
 .hue(()=>document.body.scrollTop / 1000)
   .scale(() => (Math.sin(-document.body.scrollTop / 200) + 1) * 1)
-  .modulatePixelate(noise(8,0.3).pixelate(32,32).thresh(0.4, 0.2), -1000+32, 1000).out()
+  .modulatePixelate(noise(8,0.3).pixelate(32,32).thresh(0.4, 0.2), -1000+32, 1000).layer(
+    src(s0).repeat(3,3).mask(
+      shape(4,0.5,0).scale(1,1,2).repeat(3,3,0.5).scale(1,3).sub(src(s0).repeat(3,3))
+    )
+  ).out()
